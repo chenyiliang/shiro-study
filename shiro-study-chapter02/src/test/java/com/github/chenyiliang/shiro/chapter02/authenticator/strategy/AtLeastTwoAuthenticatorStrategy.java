5df065fdda09b531c366dfd9c7a336402f89cea7
chenyiliang <chenyiliangconan@qq.com>
2016-05-25 11:15:57 +0800

    shiro 学习 首先学完chapter02

---
+++ shiro-study-chapter02/src/test/java/com/github/chenyiliang/shiro/chapter02/authenticator/strategy/AtLeastTwoAuthenticatorStrategy.java
@@ -0,0 +1,22 @@
+package com.github.chenyiliang.shiro.chapter02.authenticator.strategy;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.pam.AbstractAuthenticationStrategy;
+import org.apache.shiro.util.CollectionUtils;
+
+public class AtLeastTwoAuthenticatorStrategy extends AbstractAuthenticationStrategy {
+	@Override
+	public AuthenticationInfo afterAllAttempts(AuthenticationToken token, AuthenticationInfo aggregate)
+			throws AuthenticationException {
+		if (aggregate == null || CollectionUtils.isEmpty(aggregate.getPrincipals())
+				|| aggregate.getPrincipals().getRealmNames().size() < 2) {
+			throw new AuthenticationException("Authentication token of type [" + token.getClass() + "] "
+					+ "could not be authenticated by any configured realms.  Please ensure that at least two realm can "
+					+ "authenticate these tokens.");
+		}
+
+		return aggregate;
+	}
+}
