1fc0771cbeef5c1d7d5eccffe40e367b5fe5b71c
chenyiliang <chenyiliangconan@qq.com>
2016-06-09 00:26:52 +0800

    角色 权限 页面

---
+++ shiro-study-chapter07/src/main/java/com/github/chenyiliang/shiro/chapter07/web/servlet/UnAuthorizedServlet.java
@@ -0,0 +1,24 @@
+package com.github.chenyiliang.shiro.chapter07.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+@WebServlet(name = "unauthorizedServlet", urlPatterns = "/unauthorized")
+public class UnAuthorizedServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Subject subject = SecurityUtils.getSubject();
+		request.setAttribute("subject", subject);
+		request.getRequestDispatcher("/WEB-INF/jsp/unauthorized.jsp").forward(request, response);
+	}
+
+}
