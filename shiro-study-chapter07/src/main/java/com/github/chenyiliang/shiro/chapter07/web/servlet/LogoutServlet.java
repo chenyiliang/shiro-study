77369e3412eaed0852209db15f17538fc7c160bb
chenyiliang <chenyiliangconan@qq.com>
2016-06-09 14:13:26 +0800

    登出

--- shiro-study-chapter07/src/main/java/com/github/chenyiliang/shiro/chapter07/web/servlet/LogoutServlet.java
+++ shiro-study-chapter07/src/main/java/com/github/chenyiliang/shiro/chapter07/web/servlet/LogoutServlet.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.SecurityUtils;
+
 @WebServlet(name = "logoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		SecurityUtils.getSubject().logout();
 		request.getRequestDispatcher("/WEB-INF/jsp/logoutSuccess.jsp").forward(request, response);
 	}
 
