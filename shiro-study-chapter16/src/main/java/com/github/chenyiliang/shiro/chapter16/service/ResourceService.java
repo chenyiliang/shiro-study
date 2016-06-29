9e07407504d8b82c61aff6ecac26fbd1b439202d
chenyiliang <chenyiliangconan@qq.com>
2016-06-29 10:15:25 +0800

    chapter16 基本启动运行正常 但代码还没空细看

---
+++ shiro-study-chapter16/src/main/java/com/github/chenyiliang/shiro/chapter16/service/ResourceService.java
@@ -0,0 +1,35 @@
+package com.github.chenyiliang.shiro.chapter16.service;
+
+import java.util.List;
+import java.util.Set;
+
+import com.github.chenyiliang.shiro.chapter16.entity.Resource;
+
+public interface ResourceService {
+
+	Resource createResource(Resource resource);
+
+	Resource updateResource(Resource resource);
+
+	void deleteResource(Long resourceId);
+
+	Resource findOne(Long resourceId);
+
+	List<Resource> findAll();
+
+	/**
+	 * 得到资源对应的权限字符串
+	 * 
+	 * @param resourceIds
+	 * @return
+	 */
+	Set<String> findPermissions(Set<Long> resourceIds);
+
+	/**
+	 * 根据用户权限得到菜单
+	 * 
+	 * @param permissions
+	 * @return
+	 */
+	List<Resource> findMenus(Set<String> permissions);
+}
---
+++ shiro-study-chapter16/src/main/java/com/github/chenyiliang/shiro/chapter16/web/Constants.java
@@ -0,0 +1,5 @@
+package com.github.chenyiliang.shiro.chapter16.web;
+
+public class Constants {
+	public static final String CURRENT_USER = "user";
+}
