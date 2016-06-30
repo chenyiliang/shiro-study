8c447367136995e92096b78e44b180cf6e8d0251
chenyiliang <chenyiliangconan@qq.com>
2016-06-30 21:09:05 +0800

    验证码测试通过

---
+++ shiro-study-chapter22/src/main/java/com/github/chenyiliang/shiro/chapter22/jcaptcha/MyFormAuthenticationFilter.java
@@ -0,0 +1,18 @@
+package com.github.chenyiliang.shiro.chapter22.jcaptcha;
+
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
+
+	@Override
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response, Object mappedValue)
+			throws Exception {
+		if (request.getAttribute(getFailureKeyAttribute()) != null) {
+			return true;
+		}
+		return super.onAccessDenied(request, response, mappedValue);
+	}
+}
